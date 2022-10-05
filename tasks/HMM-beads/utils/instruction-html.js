39358c27f1dd7238fa5329f8883b9af1102cb870
jacobaparker <jacobparker659@gmail.com>
2022-10-05 17:19:22 -0400

    change all \lib directories to \utils because of the gitignore file

---
+++ tasks/HMM-beads/utils/instruction-html.js
@@ -0,0 +1,9 @@
+var beads_instructions_1_text = function() {
+  var html_arr = [
+    `<div align=center>In this task, you will see black or white beads being drawn (pulled) from jars.<br><br>On each trial, a bead will be drawn from one of two jars. Once you see this bead, your goal is to predict the color of the <b>NEXT</b> bead.<br><br>Every time you correctly guess the color of the <b>NEXT</b> bead, you will get a point. The more points you get, the higher your bonus will be at the end of the experiment.<br><br></div>`,
+    `<div align=justify>There are two types of jars:<br><br>1.   Jars filled with 80% white beads and 20% black beads.<br><br>2.   Jars filled with 20% white beads and 80% black beads.<br><br></div>`,
+    `<div align=center><img src="tasks/HMM-beads/Images/WhiteJar_HMM.png" width="200"><br><br>This is a jar filled with 80% white beads and 20% black beads.<br><br></div>`,
+    `<div align=center><img src="tasks/HMM-beads/Images/BlackJar_HMM.png" width="200"><br><br>This is a jar filled with 20% white beads and 80% black beads.<br><br></div>`
+  ]
+  return html_arr
+}
